80d27dc59efdb874b479bd3417436fbd3df00c4f
minu-arch <mezu.mezu@hotmail.com>
2024-09-28 14:25:06 +0300

    MenuNextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Login from "@/app/auth/login/page";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Login />
+    </div>
+  );
+}
