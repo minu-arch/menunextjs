1a3de29c325274b33036e3b50e910b608064c29d
minu-arch <mezu.mezu@hotmail.com>
2024-10-11 13:55:41 +0300

    loginForm Updated

--- src/app/components/auth/LoginForm.tsx
+++ src/app/components/auth/LoginForm.tsx
@@ -17,7 +17,7 @@ const LoginForm = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const response = await fetch("/api/login", {
+    const response = await fetch("https://menunextjs.vercel.app/api/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
