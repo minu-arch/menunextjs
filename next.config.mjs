23d33a9ab998cf32434882f4841ac0359f4b78b4
minu-arch <mezu.mezu@hotmail.com>
2024-10-13 16:38:24 +0300

    next.config updated

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        destination: "https://www.minufy.site/$1",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
